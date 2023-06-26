538b12d93aed80c893c5ef94b0da53a53e562936
Elvin Nikolas <elvinnikolas09@gmail.com>
2023-06-27 00:53:21 +0700

    setup layout & auth, connect mongodb, add navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,28 @@
-import '@styles/globals.css';
+import '@styles/globals.css'
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
 export const metadata = {
-    title: 'Promptshare',
-    description: 'Share & Search AI Prompts',
+  title: 'Promptshare',
+  description: 'Share & Search AI Prompts',
 }
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
-    <html lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
-            <main className="app">
-                {children}
-            </main>
-        </body>
+    <html lang='en'>
+      <body>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
+
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
   )
 }
---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+let isConnected = false
+
+export const connectDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if (isConnected) {
+        console.log('MongoDB Connected')
+        return
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'promptshare',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true
+        console.log('MongoDB Connected')
+
+    } catch (error) {
+        console.log(error)
+    }
+}
